05658c84c99f1ae3555554690cc41072880829e7
Trevor McGrath <trevormcgrath88@gmail.com>
2016-03-04 16:56:09 -0800

    Small css changes for alignment. Began script.js functionality.

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+//$(document).ready();
+
+$(document).one('pageinit', function(){
+
+	// Adding a run handler
+	$('#submit-run').on('tap', addRun); // jQuery mobile equivalent of on click
+
+	/* addRun -
+	 * The function for adding a run object
+	 *
+	 */
+	 function addRun(){
+	 	// Get values from the form. In this case, the variable miles and the date.
+	 }
+
+
+})
