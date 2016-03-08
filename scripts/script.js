f5f8b3531f544fc1571a9145fa83fe72bc6a5ba3
Trevor McGrath <trevormcgrath88@gmail.com>
2016-03-07 16:32:52 -0800

    Put in the Add a Run functionality to the JS file. If you look at Developer Tools > Resources > localstorage it's logging the run key value pairs

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,61 @@
 //$(document).ready();
 
-$(document).one('pageinit', function(){
+$(document).one('pagecreate', function(){
 
 	// Adding a run handler
-	$('#submit-run').on('tap', addRun); // jQuery mobile equivalent of on click
+	$('#submit-run').on('click', addRun); //when the add runs page button is clicked, do this function
 
 	/* addRun -
 	 * The function for adding a run object
-	 *
 	 */
 	 function addRun(){
 	 	// Get values from the form. In this case, the variable miles and the date.
+	 	var miles = $('#add-miles').val();
+	 	var date = $('#add-date').val();
+
+	 	// Create the 'run' object to be put into localstorage
+	 	var run = {
+	 		date: date,
+	 		miles: parseFloat(miles) //parseFloat is used here because natively it will save miles as a string. I want it as a decimal number.
+	 	};
+
+	 	// a function that grabs the current runs off of the localstorage
+	 	var localstorageRuns = getRunsObject(); // now have sorted objects in this variable
+
+	 	// Add new values (runs) to the object
+	 	localstorageRuns.push(run);
+	 	alert("Run Added!");
+
+	 	// Put the run you just added into localstorage, need to make a string first
+	 	localStorage.setItem('localstorageRuns', JSON.stringify(localstorageRuns));
+
+	 	// when a run is added to localstorage, return user to the homepage
+	 	window.location.href="index.html";
+
+	 	return false;
+
 	 }
 
+	 /* getRunsObject -
+	  * Logic for above function that grabs current runs off of the localstorage
+	  *
+	  */
+	 function getRunsObject (){
 
-})
+	  	// Set localstorageRuns array
+	  	var localstorageRuns = new Array();
+
+	  	// Get current runs off of local storage
+	  	var currentRuns = localStorage.getItem('localstorageRuns');
+
+	  	// Check to see if what currentRuns pulls off of localstorage is empty or not
+	  	if(currentRuns!=null){ // So, if what is pulled off IS NOT empty, then...
+	  		var localstorageRuns = JSON.parse(currentRuns); //turn what is grabbed into JSON data
+	  	}
+
+	  	// Return the localstorageRuns object
+	  	// Sort by newest date first
+	  	return localstorageRuns.sort(function(a, b){return new Date(b.date) - new Date(a.date)});
+	 }
+
+});
